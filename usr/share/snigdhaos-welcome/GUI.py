ab2db89916b8588115bef08675d49d1f7aac3f67
Eshan Roy (Eshanized) <src.eshan@gmail.com>
2023-12-16 15:39:33 +0530

    fix

--- usr/share/snigdhaos-welcome/GUI.py
+++ usr/share/snigdhaos-welcome/GUI.py
@@ -16,7 +16,7 @@ username = getpass.getuser()
 if DEBUG:
     user = username
 else:
-    user = "liveuser"
+    user = "snigdha"
 
 Settings = home + "/.config/snigdhaos-welcome/settings.conf"
 Skel_Settings = "/etc/skel/.config/snigdhaos-welcome/settings.conf"
