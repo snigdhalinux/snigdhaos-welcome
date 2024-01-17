8f62d7d5ec9cdf57b7aa52d9eb7245be459daac7
Eshan Roy (Eshanized) <src.eshan@gmail.com>
2024-01-17 16:32:03 +0600

    REMOTE_SERVER -> SnigdhaOS.Org

--- usr/share/snigdha-welcome/snigdhaos-welcome.py
+++ usr/share/snigdha-welcome/snigdhaos-welcome.py
@@ -20,7 +20,7 @@ gi.require_version("Gtk", "3.0")
 gi.require_version("Wnck", "3.0")
 from gi.repository import Gtk, GdkPixbuf, GLib, Wnck  # noqa
 
-REMOTE_SERVER = "www.google.com"
+REMOTE_SERVER = "www.snigdhaos.org"
 
 
 class Main(Gtk.Window):
